53a9b81b1905bf702375798ecc4094c051360500
Doylee <lidongyun@shuwang-tech.com>
2018-08-15 21:50:43 +0800

    * 添加了回车发送消息 * 优化了前端代码
    Signed-off-by: Doylee <lidongyun@shuwang-tech.com>

--- public/static/wiki/js/index.js
+++ public/static/wiki/js/index.js
@@ -1,14 +1,12 @@
-layui.use('element', function(){
-    var element = layui.element;
+// -=-=-=-=-=-=生成唯一标识用于ws-=-=-=-=-=-
+/**
+ * 用于生成uuid
+ * @return {string}
+ */
+function S4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
 
-    //一些事件监听
-    // element.on('tab(demo)', function(data){
-    //     console.log(data);
-    // });
-});
-
-layui.use('form', function(){
-    var form = layui.form;
-
-    //各种基于事件的操作，下面会有进一步介绍
-});
+function guuid() {
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
